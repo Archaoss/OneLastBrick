7dce90128c5534a473ec3ba2fd5aebf4e0fa16c0
Archaoss <archaoss@live.fr>
2024-09-30 18:01:16 +0200

    dev

--- src/scripts/walls.gd
+++ src/scripts/walls.gd
@@ -3,3 +3,10 @@ extends StaticBody2D
 
 @onready var viewport_rect := get_viewport_rect()
 @export var corner_offset := 20 # px
+
+
+func _ready() -> void:
+	$TopLeft    .shape.distance = corner_offset
+	$TopRight   .shape.distance = corner_offset
+	$BottomLeft .shape.distance = corner_offset
+	$BottomRight.shape.distance = corner_offset
