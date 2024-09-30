f0737aa0b7aa1bd7d39fd62e29188cb33e4c80db
erwan-privat <archaoss@live.com>
2024-10-01 01:25:16 +0200

    sprites for corners

--- src/scripts/walls.gd
+++ src/scripts/walls.gd
@@ -1,12 +1,29 @@
+@tool
 extends StaticBody2D
 
 
-@onready var viewport_rect := get_viewport_rect()
-@export var corner_offset := 20 # px
+# @onready var viewport_rect := get_viewport_rect()
+@export var corner_offset := 20:
+	set(v):
+		corner_offset = v
+		# if self.is_node_ready():
+		update_corners()
 
 
 func _ready() -> void:
+	update_corners()
+
+
+func update_corners() -> void:
+	if not is_node_ready():
+		return
+
 	$TopLeft    .shape.distance = corner_offset
 	$TopRight   .shape.distance = corner_offset
 	$BottomLeft .shape.distance = corner_offset
 	$BottomRight.shape.distance = corner_offset
+
+	var sc := corner_offset * sqrt(2)
+
+	for sp in [$SpriteTL, $SpriteTR, $SpriteBL, $SpriteBR]:
+		sp.scale = Vector2(sc, sc)
